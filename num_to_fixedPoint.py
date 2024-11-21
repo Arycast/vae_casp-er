a49f982b273172ee4b8d833227fb8e97f60846c1
Fauzan Ibrahim <fauzan.ibrahim21@gmail.com>
2024-11-21 12:04:46 +0700

    Fixed program to convert floating point to fixed point

--- num_to_fixedPoint.py
+++ num_to_fixedPoint.py
@@ -1,40 +1,75 @@
-def to_fixed_point(value, fractional_bits, exponent_bits):
-    # Check if the value is zero (special case)
-    if value == 0:
-        return '0.' + '0' * fractional_bits
+import math
+def pos_float16_to_bin(num):
+    b = ['0'] * 16
+    dec_num = int(num)
+    exp = 8; frac = 7
+    float_num = num - dec_num
+    #print(float_num, dec_num)
 
-    # Find the exponent (shift left or right) and scale the value accordingly
-    exponent = 0
-    scaled_value = value
-    
-    # Normalize the value to fit the fixed-point format with the specified fractional bits
-    while abs(scaled_value) >= 1:
-        scaled_value /= 2
-        exponent += 1
-    
-    while abs(scaled_value) < 0.5:
-        scaled_value *= 2
-        exponent -= 1
+    b[15] = '0'
 
-    # Scale the value to fit the fixed-point representation
-    fixed_value = round(scaled_value * (2 ** fractional_bits))
+    while(dec_num > 0):
+        if (dec_num % 2 == 0) : b[exp] = '0'
+        else : b[exp] = '1'
+        #print(dec_num)
+        exp += 1
+        dec_num //= 2
 
-    # Convert the fixed value to binary (integer part)
-    binary_value = bin(fixed_value)[2:]  # Remove the '0b' prefix
-    
-    # Ensure the binary representation fits the required number of fractional bits
-    binary_value = binary_value.zfill(fractional_bits + len(binary_value) - binary_value.find('1'))
-    
-    # Split the binary value into integer and fractional parts
-    integer_part = binary_value[:-fractional_bits] if len(binary_value) > fractional_bits else '0'
-    fractional_part = binary_value[-fractional_bits:]
-    
-    # Prepare the exponent part
-    # Use a fixed-width signed representation for the exponent
-    exponent_binary = bin(exponent & ((1 << exponent_bits) - 1))[2:].zfill(exponent_bits)
+    for i in range (7, -1, -1):
+        float_num *= 2
+        if (float_num == 1): 
+            b[frac] = '1'
+            break
+        elif (float_num > 1):
+            b[frac] = '1'
+            float_num = float_num - int(float_num)
+        else:
+            b[frac] = '0'
+        frac -= 1
+    #print(b)
+    return b
 
-    # Combine the parts into a final fixed-point binary representation
-    fixed_point_binary = f"{integer_part}.{fractional_part} (Exponent: {exponent_binary})"
-    
-    return fixed_point_binary
+def to_fixed_point(num):
+   temp_num = 0
+   
+   if (num < 0): temp_num = -num
+   else: temp_num = num
+   b = pos_float16_to_bin(temp_num)
 
+   flag = 0
+   final = ['0'] * len(b)
+   
+   for i in range(0, 8): final[i] = b[i]
+
+   if (num < 0):
+      for i in range (0, len(b)):
+         if (b[i] == '1'):
+            if (flag == 0):
+               flag = 1
+               final[i] = b[i]
+            else:
+               if (b[i] == '0'): final[i] = '1'
+               else: final[i] = '0'
+         else:
+            if (flag == 0):
+               
+               final[i] = b[i]
+            else:
+               if (b[i] == '0'): final[i] = '1'
+               else: final[i] = '0'
+   else: final = b
+   result = ""
+   dec = 0
+
+   #print(final)
+
+   for i in range(15,-1,-1):
+      if (i == 15 or i == 8): result += str(final[i]) + '_'
+      else: result += str(final[i])
+      dec += int(final[i]) * (2**i)
+   
+   return [result, dec]
+
+num = 1.75
+res_bin,res_dec = to_fixed_point(num)
+print(res_bin,res_dec)
