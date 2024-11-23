7d32ac57b3f8671a89a6212d5db8e22b6750436b
azkadhika3 <azkadhika3@gmail.com>
2024-11-23 18:28:41 +0700

    Update Case

--- softplus_piecewise_approx/rtl/grad_softplus.v
+++ softplus_piecewise_approx/rtl/grad_softplus.v
@@ -11,29 +11,29 @@ module grad_softplus (
    output reg [15:0] grad
 );
 
-	wire [2:0] x;
+	wire [6:0] x;
 	wire sign;
 	reg [15:0] outpos;
 	reg [15:0] outneg;
 	assign sign = operand[15];
-	assign x = operand[10:8];
+	assign x = operand[14:8];
 
 	always @ (*) begin
 		case(x)
-		  3'b000   	: outpos = 16'h0044; 		// 0
-		  3'b001   	: outpos = 16'h005a; 		// 1
-		  3'b010   	: outpos = 16'h0066; 		// 2
-		  3'b011   	: outpos = 16'h006b; 		// 3
-		  3'b100   	: outpos = 16'h006d; 		// 4
+		  7'h00   	: outpos = 16'h0044; 		// 0
+		  7'h01   	: outpos = 16'h005a; 		// 1
+		  7'h02   	: outpos = 16'h0066; 		// 2
+		  7'h03   	: outpos = 16'h006b; 		// 3
+		  7'h04   	: outpos = 16'h006d; 		// 4
 		  default  	: outpos = 16'h006e; 		// Default
 		endcase
 		
 		case(x)
-		  3'b111   	: outneg = 16'h0001; 		// -1
-		  3'b110   	: outneg = 16'h0003; 		// -2
-		  3'b101   	: outneg = 16'h0008; 		// -3
-		  3'b100   	: outneg = 16'h0014; 		// -4
-		  3'b011   	: outneg = 16'h002a; 		// -5
+		  7'hff   	: outneg = 16'h0001; 		// -1
+		  7'hfe   	: outneg = 16'h0003; 		// -2
+		  7'hfd   	: outneg = 16'h0008; 		// -3
+		  7'hfc   	: outneg = 16'h0014; 		// -4
+		  7'bhfb   	: outneg = 16'h002a; 		// -5
 		  default  	: outneg = 16'h0000; 		// Default
 		endcase
 		
--- softplus_piecewise_approx/rtl/off_softplus.v
+++ softplus_piecewise_approx/rtl/off_softplus.v
@@ -6,7 +6,6 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module off_softplus (
-    
    input [15:0] operand, 
    output reg [15:0] offset
 );
@@ -16,24 +15,24 @@ module off_softplus (
 	reg [15:0] outpos;
 	reg [15:0] outneg;
 	assign sign = operand[15];
-	assign x = operand[10:8];
+	assign x = operand[14:8];
 
 	always @ (*) begin
 		case(x)
-		  3'b000   	: outpos = 16'h004d; 		// 0
-		  3'b001   	: outpos = 16'h0037; 		// 1
-		  3'b010   	: outpos = 16'h001f; 		// 2
-		  3'b011   	: outpos = 16'h000f; 		// 3
-		  3'b100   	: outpos = 16'h0007; 		// 4
+		  7'h00   	: outpos = 16'h004d; 		// 0
+		  7'h01  	: outpos = 16'h0037; 		// 1
+		  7'h02   	: outpos = 16'h001f; 		// 2
+		  7'h03   	: outpos = 16'h000f; 		// 3
+		  7'h04   	: outpos = 16'h0007; 		// 4
 		  default  	: outpos = 16'h004d; 		// Default
 		endcase
 		
 		case(x)
-		  3'b111   	: outneg = 16'h004d; 		// -1
-		  3'b110   	: outneg = 16'h0037; 		// -2
-		  3'b101   	: outneg = 16'h001f; 		// -3
-		  3'b100   	: outneg = 16'h000f; 		// -4
-		  3'b011   	: outneg = 16'h0007; 		// -5
+		  7'hff   	: outneg = 16'h004d; 		// -1
+		  7'hfe   	: outneg = 16'h0037; 		// -2
+		  7'hfd   	: outneg = 16'h001f; 		// -3
+		  7'hfc   	: outneg = 16'h000f; 		// -4
+		  7'hfb   	: outneg = 16'h0007; 		// -5
 		  default  	: outneg = 16'h0002; 		// Default
 		endcase
 		
