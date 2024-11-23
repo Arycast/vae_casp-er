82ea2e804dc71789a3a924652f394397ed8800d4
AchmadKuddah <13221057@std.stei.itb.ac.id>
2024-11-23 17:46:41 +0700

    seperated softplus

--- softplus_piecewise_approx/rtl/grad_softplus.v
+++ softplus_piecewise_approx/rtl/grad_softplus.v
@@ -8,8 +8,7 @@
 module grad_softplus (
     
    input [15:0] operand, 
-   output reg [15:0] grad,
-   output reg [15:0] offset
+   output reg [15:0] grad
 );
 
 	wire [2:0] x;
@@ -41,31 +40,6 @@ module grad_softplus (
 		case(sign)
 		  0   		: grad = outpos; 		// Positve
 		  default   : grad = outneg; 		// Default
-		endcase
-	end
-
-	always @ (*) begin //offset
-		case(x)
-		  3'b000   	: outpos = 16'h004D; 		// 0
-		  3'b001   	: outpos = 16'h0037; 		// 1
-		  3'b010   	: outpos = 16'h001F; 		// 2
-		  3'b011   	: outpos = 16'h000F; 		// 3
-		  3'b100   	: outpos = 16'h0007; 		// 4
-		  default  	: outpos = 16'h0003; 		// Default
-		endcase
-		
-		case(x)
-		  3'b111   	: outneg = 16'h004D; 		// -1
-		  3'b110   	: outneg = 16'h0037; 		// -2
-		  3'b101   	: outneg = 16'h001F; 		// -3
-		  3'b100   	: outneg = 16'h000F; 		// -4
-		  3'b011   	: outneg = 16'h0007; 		// -5
-		  default  	: outneg = 16'h0000; 		// Default
-		endcase
-		
-		case(sign)
-		  0   		: offset = outpos; 		// Positve
-		  default   : offset = outneg; 		// Default
 		endcase
 	end
 endmodule
